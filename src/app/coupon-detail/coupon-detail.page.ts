d570a3f0a5f782080ad5b6a9b5a6fcc0942a5929
Ramiro Olivencia <rolivencia@frsf.utn.edu.ar>
2019-10-26 18:38:10 -0300

    Agregada generación de código en base a objetos customer y coupon. Corrección en typo de script de build.

--- src/app/coupon-detail/coupon-detail.page.ts
+++ src/app/coupon-detail/coupon-detail.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute, Router } from "@angular/router";
 import { Coupon } from "@app/_models/coupon";
 import { ThemeService } from "@app/_services/theme.service";
+import { AuthService } from "@app/_services/auth.service";
 
 @Component({
   selector: "app-coupon-detail",
@@ -9,13 +10,15 @@ import { ThemeService } from "@app/_services/theme.service";
   styleUrls: ["./coupon-detail.page.scss"]
 })
 export class CouponDetailPage implements OnInit {
-
   coupon: Coupon;
   buttonLabel: string = "Ver QR para Canjear";
   view: string = "detail";
   redeemed: boolean = false;
 
+  couponCustomerCode: string;
+
   constructor(
+    private authService: AuthService,
     private route: ActivatedRoute,
     private router: Router,
     public themeService: ThemeService
@@ -27,8 +30,17 @@ export class CouponDetailPage implements OnInit {
     }
   }
 
+  ngAfterViewInit(){
+    this.generateQrCode();
+  }
+
   changeCouponView() {
     this.view = this.view === "detail" ? "redeem" : "detail";
-    this.buttonLabel = this.view === "detail" ? "Ver QR para Canjear" : "Volver";
+    this.buttonLabel =
+      this.view === "detail" ? "Ver QR para Canjear" : "Volver";
+  }
+
+  generateQrCode(){
+    this.couponCustomerCode = JSON.stringify({customer: this.authService.currentCustomer, coupon: this.coupon});
   }
 }
