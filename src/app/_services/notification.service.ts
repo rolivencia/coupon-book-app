daddefa5d50cd5b0cef29e04c2ef5b93e5ca7c8b
Ramiro Olivencia <rolivencia@frsf.utn.edu.ar>
2019-12-12 19:44:45 -0300

    Notificaciones: Agregadas rutas para enviar al usuario hacia cupones o recomendados

--- src/app/_services/notification.service.ts
+++ src/app/_services/notification.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { OneSignal } from '@ionic-native/onesignal/ngx';
+import { Router } from "@angular/router";
 
 @Injectable({
   providedIn: 'root'
@@ -22,7 +23,7 @@ export class NotificationService {
   private _authKey: string = "MDFlN2NjNjctMGY1MC00ZTVmLThhOTEtMWJmZTI3NDI4YzFk";
   private _senderId: string = "287785352097";
 
-  constructor(private oneSignal: OneSignal) { }
+  constructor(private oneSignal: OneSignal, private router: Router) { }
 
   init(){
     // I recommend to put these into your environment.ts
@@ -35,7 +36,6 @@ export class NotificationService {
       let msg = data.payload.body;
       let title = data.payload.title;
       let additionalData = data.payload.additionalData;
-      this.showAlert(title, msg, additionalData.task);
     });
 
     // Notification was really clicked/opened
@@ -43,25 +43,24 @@ export class NotificationService {
       // Just a note that the data is a different place here!
       let additionalData = data.notification.payload.additionalData;
 
-      this.showAlert('Notification opened', 'You already read this before', additionalData.task);
+      this.navigate('Notification opened', 'You already read this before', additionalData);
     });
 
     this.oneSignal.endInit();
   }
 
-  async showAlert(title, msg, task) {
-    // const alert = await this.alertCtrl.create({
-    //   header: title,
-    //   subHeader: msg,
-    //   buttons: [
-    //     {
-    //       text: `Action: ${task}`,
-    //       handler: () => {
-    //         // E.g: Navigate to a specific screen
-    //       }
-    //     }
-    //   ]
-    // });
-    // alert.present();
+  async navigate(title, msg, data) {
+    const route = data.route;
+    const id = data.id;
+    if(route === "coupons"){
+      this.router.navigate(["/coupons"]);
+    }
+    if(route === "coupon-detail"){
+      this.router.navigate(["/coupon-detail/" + id]);
+    }
+    if(route === "recommended"){
+      this.router.navigate(["/recommended"]);
+
+    }
   }
 }
